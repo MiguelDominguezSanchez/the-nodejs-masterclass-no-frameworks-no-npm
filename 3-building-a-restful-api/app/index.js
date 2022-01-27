832146bbea78fb3308c523caf58e079c98e00eb4
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-01-27 14:39:11 +0100

    3-20. Adding Configuration

--- 3-building-a-restful-api/app/index.js
+++ 3-building-a-restful-api/app/index.js
@@ -7,6 +7,7 @@
 var http = require('http')
 var url = require('url')
 var StringDecoder = require('string_decoder').StringDecoder
+var config = require('./config')
 
 // The server should respond tto all requests with  a string
 var server = http.createServer(function (req, res) {
@@ -72,9 +73,15 @@ var server = http.createServer(function (req, res) {
 	})
 })
 
-// Start thee server, and have it listen on port 3000
-server.listen(3000, function () {
-	console.log('The server is listening on port 3000 now')
+// Start thee server
+server.listen(config.port, function () {
+	console.log(
+		'The server is listening on port ' +
+			config.port +
+			' in ' +
+			config.envName +
+			' mode'
+	)
 })
 
 // Define the handlers
